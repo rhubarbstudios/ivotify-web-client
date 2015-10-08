09a39705da24ce916726bcf45d4adbdbd86ebd9e
Alex Dinari <alexdinari@roadhouseapps.com>
2015-10-08 15:17:41 -0700

    added angular-materialize

--- gulp/styles.js
+++ gulp/styles.js
@@ -17,7 +17,7 @@ gulp.task('styles', function () {
   };
 
   var injectFiles = gulp.src([
-    path.join(conf.paths.src, '/app/**/*.scss'),
+    path.join(conf.paths.src, '/app/stylesheets/*.scss'),
     path.join('!' + conf.paths.src, '/app/index.scss')
   ], { read: false });
 
