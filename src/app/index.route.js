1a9963e8226319857210db147e1e436c2aeeb7c4
Alex Dinari <alexdinari@roadhouseapps.com>
2015-10-08 14:37:14 -0700

    edited index.route.js file to reflect the correct routes

--- src/app/index.route.js
+++ src/app/index.route.js
@@ -1,21 +1,40 @@
-(function() {
-  'use strict';
+(function(){
+'use strict';
 
   angular
     .module('ivotifyFrontend')
-    .config(routeConfig);
+    .config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider){
 
-  /** @ngInject */
-  function routeConfig($stateProvider, $urlRouterProvider) {
     $stateProvider
-      .state('home', {
-        url: '/',
-        templateUrl: 'app/main/main.html',
-        controller: 'MainController',
-        controllerAs: 'main'
+      .state('welcome', {
+        url:'/',
+        templateUrl: 'app/templates/welcome.html',
+        controller: 'WelcomeController',
+        controllerAs:'welcomectrl'
+      })
+      .state('admin', {
+        url: '/admin',
+        templateUrl: 'app/templates/admin.html'
+        })
+      .state('admin.issues', {
+        url: '/issues',
+        templateUrl: 'app/templates/admin/issues.html',
+        controller: 'AdminIssuesController'
+        })
+       .state('admin.candidates', {
+        url: '/candidates',
+        templateUrl: 'app/templates/admin/candidates.html',
+        controller: 'AdminCandidatesController'
+      })
+      .state('issues', {
+        url: '/issues',
+        templateUrl: 'app/templates/issues.html',
+        controller: 'MainController'
       });
+     
+      $urlRouterProvider.otherwise('/');
+    }
 
-    $urlRouterProvider.otherwise('/');
-  }
+  ]);
 
 })();
