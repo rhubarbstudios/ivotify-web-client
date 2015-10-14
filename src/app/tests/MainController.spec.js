c927cf7f8f9b46a76ffc8ebd12eb313637537faa
Alex Dinari <alexdinari@roadhouseapps.com>
2015-10-14 11:36:46 -0700

    Tests for routes, variables in mainController and ngresource factory completed and passing

--- src/app/tests/MainController.spec.js
+++ src/app/tests/MainController.spec.js
@@ -1,101 +1,35 @@
-// "use strict"
+'use strict';
+describe('MainController', function() {
+  var     $q,
+          $controller,
+          $rootScope,
+          $scope;
 
-// describe('Resource', function() {
-//   beforeEach(module('ivotifyFrontend'));
-//   beforeEach(inject($httpBackend), function(){
-//     $httpBackend.whenGET('/api/issues').respond([]);
+beforeEach(module('ivotifyFrontend'));
 
-//     it('should return an array of objects', function(){
-//       issues = ''
-//       expect()
+  beforeEach(inject(function(_$controller_, _$q_, _$rootScope_){
+    // The injector unwraps the underscores (_) from around the parameter names when matching
+    $controller = _$controller_;
+    $q = _$q_;
+    $rootScope = _$rootScope_;
+  }));
 
-//     })
-//   });
-    
+  // Testing the $scope.issues and $scope.candidates variables
+  describe('$scope.issues + $scope.candidates', function() {
 
+     it('makes sure that $scope.issues returns an array', function() {
+      var $scope = {};
+      var controller = $controller('MainController', { $scope: $scope });
+      $scope.issues = [{}];
+      expect($scope.issues).toBeTruthy();
+     });
 
+     it('makes sure that $scope.candidates returns an array', function() {
+      var $scope = {};
+      var controller = $controller('MainController', { $scope: $scope });
+      $scope.candidates = [{}];
+      expect($scope.candidates).toBeTruthy();
+     });
 
-// }
-
-
-
-
-
-
-
-
-
-// describe('MainController', function() {
-
-//   var   $q,
-//         $controller,
-//         $rootScope,
-//         $scope,
-//         mockResourceService,
-//         mockResourceResponse = [{name: 'foobagel'}, {name: 'barbagel'}],
-//         queryDeferred;
-
-//   beforeEach(module('ivotifyFrontend'));
-
-//   beforeEach(inject(function(_$controller_, _$q_, _$rootScope_){
-//     // The injector unwraps the underscores (_) from around the parameter names when matching
-//     $controller = _$controller_;
-//     $q = _$q_;
-//     $rootScope = _$rootScope_;
-//   }));
-
-//   beforeEach(inject(function($controller) {
-//     $scope = $rootScope.$new();
-    
-//     mockResourceService = {
-//       get: function() {
-//         queryDeferred = $q.defer();
-//         return {$promise: queryDeferred.promise};
-//       }
-//     }
-    
-//     spyOn(mockResourceService, 'get').and.callThrough();
-    
-//     $controller('MainController', {
-//       '$scope': $scope,
-//       'Resource': mockResourceService
-//     });
-//   }));
-
-//   describe('mockResourceService.get', function() {
-      
-//       beforeEach(function() {
-//         queryDeferred.resolve(mockResourceResponse);
-//         $rootScope.$apply();
-//       });
-      
-//       it('should get the Resource', function() {
-//         expect(mockResourceService.get).toHaveBeenCalled();
-//       });
-      
-//       it('should set the response from the mockResourceService to $scope.issues', function() {
-//         expect($scope.issues).toEqual(mockResourceResponse);
-//       });
-//     });
-
-
-
-//   // Testing the $scope.issues and $scope.candidates variables
-//   describe('$scope.issues + $scope.candidates', function() {
-
-//      it('makes sure that $scope.issues returns an array', function() {
-//       var $scope = {};
-//       var controller = $controller('MainController', { $scope: $scope });
-//       $scope.issues = [{}];
-//       expect($scope.issues).toBeTruthy();
-//      });
-
-//      it('makes sure that $scope.candidates returns an array', function() {
-//       var $scope = {};
-//       var controller = $controller('MainController', { $scope: $scope });
-//       $scope.candidates = [{}];
-//       expect($scope.candidates).toBeTruthy();
-//      });
-
-//   });
-// });
+  });
+});
---
+++ src/app/tests/resources.spec.js
@@ -0,0 +1,28 @@
+'use strict';
+describe('Resources', function () {
+    var mockResource, 
+        $httpBackend;
+
+    beforeEach(angular.mock.module('ivotifyFrontend'));
+
+    beforeEach(function () {
+        angular.mock.inject(function ($injector) {
+            $httpBackend = $injector.get('$httpBackend');
+            mockResource = $injector.get('Resources');
+        })
+    });
+
+    describe('IssueResources.get', function () {
+        it('should retrieve an array of issue objects', inject(function (Resources) {
+            $httpBackend.expectGET('https://ivotify-api.herokuapp.com/api/issues')
+                .respond(200, 'success');
+        }));
+    });
+
+    describe('CandidateResources.get', function () {
+        it('should retrieve an array of candidate objects', inject(function (Resources) {
+            $httpBackend.expectGET('https://ivotify-api.herokuapp.com/api/candidates')
+                .respond(200, 'success');
+        }));
+    });
+});
